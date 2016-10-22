63d1ccb7f00dd590f40b651e7b11114aa148bb71
thijsvanede <thijsvanede@hotmail.com>
2016-10-22 17:36:07 -0400

    Auto-commit

--- server.js
+++ server.js
@@ -56,6 +56,41 @@ MongoClient.connect(databaseURL, function(err, db){
 /**************************************************/
 //TODO
 
+
+
+
+var MongoDBCon   = require('./server/database/database.js').MongoDBCon;
+var c = new MongoDBCon();
+c.insertDocument('restaurants', {
+      "address" : {
+         "street" : "2 Avenue",
+         "zipcode" : "10075",
+         "building" : "1480",
+         "coord" : [ -73.9557413, 40.7720266 ]
+      },
+      "borough" : "Manhattan",
+      "cuisine" : "Italian",
+      "grades" : [
+         {
+            "date" : new Date("2014-10-01T00:00:00Z"),
+            "grade" : "A",
+            "score" : 11
+         },
+         {
+            "date" : new Date("2014-01-16T00:00:00Z"),
+            "grade" : "B",
+            "score" : 17
+         }
+      ],
+      "name" : "Vella",
+      "restaurant_id" : "41704620"
+   });
+
+c.find('restaurants', {"grades.grade": "B"}, function(doc){
+  console.log(doc);
+});
+
+
 /**************************************************/
 /**                Starting server               **/
 /**************************************************/
---
+++ server/database/database.js
@@ -0,0 +1,84 @@
+/**************************************************/
+/**                   Imports                    **/
+/**************************************************/
+var MongoClient  = require('mongodb').MongoClient;
+var assert       = require('assert');
+
+/**************************************************/
+/**                  Constants                   **/
+/**************************************************/
+/* URL to initiate connection with database. */
+var url = 'mongodb://0.0.0.0:27017/SDM';
+
+/**************************************************/
+/**             Private functions                **/
+/**************************************************/
+
+/**
+ * Private method to insert document.
+ */
+var _insertDoc = function(db, collection, document, callback) {
+  db.collection(collection).insertOne(document, function(err, result){
+    assert.equal(err, null);
+    callback();
+  });
+};
+
+/**
+ * Private method to query database.
+ */
+var _find = function(db, collection, query, callback) {
+  var cursor = db.collection(collection).find(query);
+  cursor.each(function(err, doc){
+    assert.equal(err, null);
+    if(doc != null){
+      callback(doc);
+    }else {
+      callback();
+    }
+  });
+};
+
+/**************************************************/
+/**                  Constructor                 **/
+/**************************************************/
+var MongoDBCon = function () {};
+
+/**************************************************/
+/**               Public functions               **/
+/**************************************************/
+
+/**
+ * Method to insert document into collection.
+ * @param collection = collection in which to insert document.
+ * @param document = document to insert in collection.
+ */
+MongoDBCon.prototype.insertDocument = function (collection, document){
+  MongoClient.connect(url, function(err, db){
+    assert.equal(null, err);
+    _insertDoc(db, collection, document, function(){
+      db.close();      
+    })
+  });
+};
+
+/**
+ * Method to find data in collection.
+ * @param collection = collection in which to find data.
+ * @param query = query with which to find data.
+ * @param callback = callback function to call with found data.
+ */
+MongoDBCon.prototype.find = function (collection, query, callback){
+  MongoClient.connect(url, function(err, db){
+    assert.equal(null, err);
+    _find(db, collection, query, function(doc){
+      db.close();   
+      callback(doc);
+    })
+  });
+};
+
+/**************************************************/
+/**                   Export                     **/
+/**************************************************/
+exports.MongoDBCon = MongoDBCon;
